b0c160b0dafcbd54982fdfa59989c266cd834ca5
Adil <adilk676@gmail.com>
2023-03-30 15:33:01 +0100

    Added a Fake Loading State

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const filmInfo = document.querySelector(".films__grid");
+const spinner = document.querySelector(".films__fa");
+const spinnerShowing = document.querySelector(".fa-spinner");
 
 async function mainFilms(event) {
   const searchInput = event.target.value;
+  spinnerShowing.classList += " films__loading--spinner";
   const fetchFilms = await fetch(
     `https://www.omdbapi.com/?apikey=96689458&s=${searchInput}`
   );
 
   const data = await fetchFilms.json();
-  filmInfo.innerHTML = data.Search.map((film) => {
-    return showFilm(film);
-  }).join("");
+
+  return new Promise(() => {
+    setTimeout(() => {
+      if (!data.Search) {
+      } else if (data.Search) {
+        spinner.classList.remove("films__loading");
+        filmInfo.innerHTML = data.Search.map((film) => {
+          return showFilm(film);
+        }).join("");
+        spinnerShowing.classList.remove("films__loading--spinner");
+      }
+    }, 1000);
+  });
 }
 mainFilms();
 
